808b313c7c0a1b783ac8088f15adb4548a52fa3e
Daniil Yaskevich <eqxbarboss@gmail.com>
2020-04-02 13:09:05 +0300

    Implemented avatar upload part of editable profile

--- pacer/client/src/app/profile-screen/editableUserProfile.js
+++ pacer/client/src/app/profile-screen/editableUserProfile.js
@@ -2,11 +2,16 @@ import React from 'react';
 
 import {ModalWindow} from '../common-components/commonComponents';
 import {UserDataForm} from '../common-components/commonComponents';
+import {RoundImage} from '../common-components/commonComponents';
 
 class EditableUserProfile extends React.Component{
     constructor(props) {
       super(props);
       this.handleSubmit = this.handleSubmit.bind(this);
+      this.handleUpdateAvatar = this.handleUpdateAvatar.bind(this);
+      this.handleInputChange = this.handleInputChange.bind(this);
+
+      this.state = {imgFileName: ""};
     }
   
     handleSubmit(userData) {
@@ -14,7 +19,24 @@ class EditableUserProfile extends React.Component{
       // check userData and call some back-end
       console.log(userData)
     }
+
+    handleUpdateAvatar(event) {
+      // upload image to server
+      console.log(this.state.imgFileName);
+
+      event.preventDefault();
+    }
+    
+    handleInputChange(event) {
+      const target = event.target;
   
+      this.setState({
+        [target.name]: target.value
+      });
+  
+      event.preventDefault();
+    }
+
     render() {
       const user = this.props.user;
       const userData = {  email: user.email
@@ -25,6 +47,15 @@ class EditableUserProfile extends React.Component{
       return (
         <ModalWindow onClose={this.props.onClose}>
           <h2 className="AlignedItem">Edit Profile</h2>
+          <div className="AlignedItem">
+            <div className="Aligner">
+              <RoundImage imgUrl={user.imgUrl} alt="User avatar"/>
+            </div>
+          </div>
+          <form className="AlignedItem Aligner" onSubmit={this.handleUpdateAvatar}>
+            <input className="FormAlignedItem FormInput" value={this.state.imgFileName} name="imgFileName" type="file" onChange={this.handleInputChange}/>
+            <input className="FormAlignedItem FormInput" type="submit" value="Upload"/>
+          </form>
           <UserDataForm submitText="Save" onSubmit={this.handleSubmit} userData={userData}/>
         </ModalWindow>
       );
