0e2809074c9af36fa587e75c593ad2dcd0302a26
KainWhite <kain.svetly@gmail.com>
2020-04-14 14:39:52 +0300

    Add users nearby coordinates

--- pacer/client/src/app/main-app/map/map.js
+++ pacer/client/src/app/main-app/map/map.js
@@ -7,8 +7,23 @@ class YandexMap extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      coordinates: [0, 0],
+      myCoordinates: [0, 0],
+      usersNearbyCoordinates: [],
     }
+  }
+
+  componentDidMount() {
+    this.serverPollCoordsTimerID = setInterval(() => {
+      /*call server for getting users coords with setstate here*/
+    }, 10000);
+    this.pushMyCoordsTimerID = setInterval(() => {
+      /*push my coords to server with setstate here*/
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.serverPollCoordsTimerID);
+    clearInterval(this.pushMyCoordsTimerID);
   }
 
   setCurrentPosition(ymaps) {
@@ -16,26 +31,32 @@ class YandexMap extends React.Component {
       provider: 'browser',
       mapStateAutoApply: true
     }).then(res => {
-       this.setState({
-         coordinates: res.geoObjects.get(0).geometry.getCoordinates(),
-       });
+      this.setState({
+       myCoordinates: res.geoObjects.get(0).geometry.getCoordinates(),
+      });
     }, err => {
       console.error("coordinates are not set due to ", err);
     });
   }
 
   render() {
+    const usersNearbyPlacemarks = this.state.usersNearbyCoordinates
+        .map(usersNearbyCoordinate =>
+          <Placemark geometry={usersNearbyCoordinate.coordinates}
+                     key={usersNearbyCoordinate.name}/>
+    );
     return (
       <div className="map">
         <YMaps query={{
                lang: "en_US",
                apikey: "f8a4a7fe-f442-4a37-af5a-a4dec57c863f"}}>
-          <Map state={{center: this.state.coordinates, zoom: 9}}
+          <Map state={{center: this.state.myCoordinates, zoom: 12}}
                height="100%"
                width="100%"
                modules={["geolocation", "geocode"]}
                onLoad={(ymaps) => {this.setCurrentPosition(ymaps);}}>
-            {this.state.coordinates && <Placemark geometry={this.state.coordinates}/>}
+            {this.state.myCoordinates && <Placemark geometry={this.state.myCoordinates}/>}
+            {usersNearbyPlacemarks}
           </Map>
         </YMaps>
       </div>
