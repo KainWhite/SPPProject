d9f44f16717186fbcf85b1adc9eeee6c35dd18be
KainWhite <kain.svetly@gmail.com>
2020-05-04 14:55:12 +0300

    Remove exception handling trash from server app.js

--- pacer/server/app.js
+++ pacer/server/app.js
@@ -1,4 +1,3 @@
-//const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
@@ -16,10 +15,6 @@ GenericDao.establishConnection();
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -33,21 +28,5 @@ app.use(authHandler);
 app.use('/login', loginRouter);
 app.use('/users', userRouter);
 app.use('/upload-image', uploadImageRouter);
-
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-//
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-//
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
 
 module.exports = app;
